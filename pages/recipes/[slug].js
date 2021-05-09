eb186b0411d9519aa05cd558f1a6c39f2ec423a9
Suhas Jagannath <sjaga003@ucr.edu>
2021-05-09 15:12:03 -0700

    Add individual pages having content

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,47 @@
-export default function RecipeDetails() {
+import { createClient } from 'contentful';
+import Image from 'next/image';
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: {
+        slug: item.fields.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug,
+  });
+
+  return {
+    props: { recipe: items[0] },
+  };
+};
+
+export default function RecipeDetails({ recipe }) {
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image />
+      </div>
     </div>
-  )
+  );
 }
